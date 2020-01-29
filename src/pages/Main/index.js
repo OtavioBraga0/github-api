e660185f3a5c9a045242b80ba3530fedb165165b
OtavioBraga0 <amexassessoria1@gmail.com>
2020-01-29 13:09:01 -0300

    Adicionado repositórios ao localStorage

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,32 +1,59 @@
 import React, { Component } from 'react';
-
-import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
 
 import api from '../../services/api';
 
-import { Container, Form, SubmitButton } from './styles';
+import { Container, Form, SubmitButton, List } from './styles';
 
 export default class Main extends Component {
   state = {
     newRepo: '',
+    repositories: [],
+    loading: false,
   };
 
-  handleInputChange = async e => {
+  componentDidMount(){
+    const repositories = localStorage.getItem('repositories');
+
+    if (repositories) {
+      this.setState({repositories: JSON.parse(repositories)})
+    }
+  }
+
+  componentDidUpdate(_, prevState){
+    const {repositories} = this.state
+
+    if(prevState.repositories !== repositories){
+      localStorage.setItem('repositories', JSON.stringify(repositories));
+    }
+  }
+
+  handleInputChange = e => {
     this.setState({ newRepo: e.target.value });
+  }
 
-    const { newRepo } = this.state;
-
-    const response = await api.get(`repos/rocketseat/${newRepo}`);
-  };
-
-  handleSubmit(e) {
+  handleSubmit = async e => {
     e.preventDefault();
 
+    this.setState({loading: true})
 
+    const { newRepo, repositories } = this.state;
+
+    const response = await api.get(`repos/${newRepo}`);
+
+    const data = {
+      name: response.data.full_name,
+    }
+
+    this.setState({
+      repositories: [...repositories, data],
+      newRepo: '',
+      loading: false,
+    })
   }
 
   render() {
-    const { newRepo } = this.state;
+    const { newRepo, repositories, loading } = this.state;
 
     return (
       <Container>
@@ -41,10 +68,21 @@ export default class Main extends Component {
             onChange={this.handleInputChange}
           />
 
-          <SubmitButton>
-            <FaPlus color="#FFF" size={14} />
+          <SubmitButton loading={loading}>
+            { loading ? <FaSpinner color="#FFF" size={14} /> : <FaPlus color="#FFF" size={14} /> }
           </SubmitButton>
         </Form>
+
+        <List>
+          {repositories.map(repository => {
+            return (
+              <li key={repository.name}>
+                <span>{repository.name}</span>
+                <a href="">Detalhes</a>
+              </li>
+            );
+          })}
+        </List>
       </Container>
     );
   }
